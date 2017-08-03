bb9c15de9dff7a138266f069d03a299c53e63a46
Esteban Arredondo <es_7eban@hotmail.com>
2017-08-03 12:34:28 +0200

    Modificacion de charset y collation base de datos, cambio de nullable del campo pais_cod en la tabla ciudades

--- database/migrations/2017_07_27_142346_create_ciudades_table.php
+++ database/migrations/2017_07_27_142346_create_ciudades_table.php
@@ -17,7 +17,7 @@ class CreateCiudadesTable extends Migration
             $table->increments('id');
             $table->char('cod',3)->unique()->comment('Codigo de la ciudad donde esta el aeropuerto /trips.data.city[].code');
             $table->string('ciudad_nom')->comment('Nombre de la ciudad donde esta el aeropuerto');
-            $table->char('pais_cod',2)->comment('Codigo de dos caracteres del pais donde esta el aeropuerto');
+            $table->char('pais_cod',2)->nullable()->comment('Codigo de dos caracteres del pais donde esta el aeropuerto');
             $table->softDeletes();
             $table->timestamps();
 
